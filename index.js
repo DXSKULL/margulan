6e3562557e7072877b38cbe1d090713dff57a7a8
DXSKULL <81633949+DXSKULL@users.noreply.github.com>
2022-02-07 12:49:26 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
-alert("Margulan Zhumabek, CS-2110");
-var today = new Date();
-var year = today.getFullYear();
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
 
-var weekdays =
-["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var day = weekdays[today.getDay()];
-
-var date = today.getDate();
-
-var month = today.getMonth() + 1;
-
-var curr = "(" + today.getHours() +":" + today.getMinutes() + ":" + today.getSeconds() + ")"
-
-alert("Year: " + year +"\n" + "Today is: " + day +"\n"  + "Date: " + date +"\n"  + "Month: " + month +"\n"  + "Current times is: " + curr +"\n");
-function freedom(CurrentDate)
-{
-	var TYear = CurrentDate.getFullYear();
-        var TDay = new Date("June, 18, 2023");
-        TDay.getFullYear(TYear);
-        var DayCount=(TDay-CurrentDate)/(1000*60*60*24);
-        DayCount=Math.round(DayCount);
-    return(DayCount);
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    
+  } else {
+    confirm_password.setCustomValidity('');
+  }
 }
 
-var Today 	= new Date();
-	var z1 = freedom(Today);
-	alert(z1 + " days left until the freedom!");
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
 
-  function multiplyBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+let input = document.querySelector(".input");
+let button = document.querySelector(".button");
+
+button.disabled = true; //setting button state to disabled
+
+function stateHandle() {
+    if (document.querySelector(".input").value === "" && document.getElementById("password").value === "" && document.getElementById("confirm_password").value === "") {
+        button.disabled = true; //button remains disabled
+    } else {
+        button.disabled = false; //button is enabled
+    }
 }
 
-function divideBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 / num2;
+confirm_password.addEventListener("change", stateHandle);
+function myFunction() {
+    alert("You have successfully registered!");
 }
